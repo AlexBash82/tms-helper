2bf25241cc3032fbdc0b6271ae3edafc1ebcc965
AlexBash82 <kometa.4m@gmail.com>
2024-12-31 15:15:17 -0800

    I can read epub

--- src/Pages/Epub/Epub.tsx
+++ src/Pages/Epub/Epub.tsx
@@ -2,27 +2,43 @@ import React, { useEffect, useRef } from 'react'
 import ePub, { Rendition } from 'epubjs'
 
 const Epub = () => {
-  const bookRef = useRef(null)
+  const bookRef = useRef<HTMLDivElement | null>(null)
   const renditionRef = useRef<Rendition | null>(null)
 
-  // Путь к файлу в папке public
-  const epubFile = '/epub/mwb_E_202411.epub'
+  const epubFile = '/mwb_E_202411.epub'
 
   useEffect(() => {
     if (epubFile && bookRef.current) {
+      console.log('Rendering book...')
       const book = ePub(epubFile)
 
-      const rendition = book.renderTo(bookRef.current, {
-        width: '100%',
-        height: '100%',
-      })
+      book.ready
+        .then(() => {
+          console.log('Book loaded successfully')
+          console.log('Table of Contents:', book.navigation.toc)
 
-      rendition
-        .display('start') // Указание начальной главы
-        .then(() => console.log('EPUB displayed successfully'))
-        .catch((error) => console.error('Error rendering EPUB:', error))
+          // Рендерим первую главу
+          const startHref =
+            book.navigation.toc[1]?.href || book.spine.get(0)?.href
 
-      renditionRef.current = rendition
+          if (!bookRef.current) {
+            console.error('Error: bookRef is null.')
+            return
+          }
+
+          renditionRef.current = book.renderTo(bookRef.current, {
+            width: '100%',
+            height: '100%',
+          })
+
+          renditionRef.current
+            .display(startHref)
+            .then(() => console.log('EPUB displayed successfully'))
+            .catch((error) => console.error('Error rendering EPUB:', error))
+        })
+        .catch((error) => {
+          console.error('Error loading book:', error)
+        })
     }
 
     return () => {
@@ -30,22 +46,12 @@ const Epub = () => {
     }
   }, [epubFile])
 
-  const nextPage = () => {
-    renditionRef.current?.next()
-  }
-
-  const prevPage = () => {
-    renditionRef.current?.prev()
-  }
-
   return (
     <div>
       <div
         ref={bookRef}
-        style={{ width: '100%', height: '500px', border: '1px solid black' }}
+        style={{ width: '1100px', height: '600px', border: '1px solid black' }}
       />
-      <button onClick={prevPage}>Previous Page</button>
-      <button onClick={nextPage}>Next Page</button>
     </div>
   )
 }
